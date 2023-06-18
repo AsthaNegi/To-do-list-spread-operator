891852ff2dbb9b1fb05672802c6864dd7e5b2816
Astha <iaanegi@gmail.com>
2023-06-18 10:48:27 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newInput = event.target.value;
+    setInputText(newInput);
+  }
+
+  function handleClick() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+
+    setInputText("");
+    console.log(items);
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item) => {
+            return <li>{item}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
